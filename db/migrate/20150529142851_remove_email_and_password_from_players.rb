cb4e34e1da4bc6f5460de145c98b6890541f26b7
narimanhesham <nariman.hesham93@gmail.com>
2015-11-22 19:32:32 +0200

    Updating Database 2

--- db/migrate/20150529142851_remove_email_and_password_from_players.rb
+++ db/migrate/20150529142851_remove_email_and_password_from_players.rb
@@ -1,6 +1,5 @@
 class RemoveEmailAndPasswordFromPlayers < ActiveRecord::Migration
   def change
-  	remove_column :players, :email
   	remove_column :players, :password
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -167,6 +167,7 @@ ActiveRecord::Schema.define(version: 20150530124006) do
     t.integer  "avatar_id",              limit: 4
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+    t.string   "email",                  limit: 255,   default: "", null: false
     t.string   "encrypted_password",     limit: 255,   default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
@@ -179,6 +180,7 @@ ActiveRecord::Schema.define(version: 20150530124006) do
   end
 
   add_index "players", ["avatar_id"], name: "index_players_on_avatar_id", using: :btree
+  add_index "players", ["email"], name: "index_players_on_email", unique: true, using: :btree
   add_index "players", ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true, using: :btree
 
   create_table "question_locations", force: :cascade do |t|
